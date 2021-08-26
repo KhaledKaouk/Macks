c635fb4a3043470f9537dab0e660b078d99d966d
KhaledKaouk <86448870+KhaledKaouk@users.noreply.github.com>
2021-08-26 14:02:19 +0300

    adding an option for development mode

--- src/app/Services/pos.service.ts
+++ src/app/Services/pos.service.ts
@@ -2,35 +2,39 @@ import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 import { Observable } from 'rxjs';
 import { POs } from '../Models/Po-model';
+import { InDevMode } from '../Utilities/Common';
 
 @Injectable({
   providedIn: 'root'
 })
 export class POsService {
 
-  constructor(private http: HttpClient,) { }
+   ApiURL: string = "https://macksdistribution.com/api/account";
+  constructor(private http: HttpClient,) {
+    if (InDevMode) this.ApiURL = "http://localhost:5000"
+   }
 
   GetPos() {
-    return this.http.get<POs>("https://macksdistribution.com/api/account/getAllPos").toPromise();
+    return this.http.get<POs>(this.ApiURL + "/getAllPos").toPromise();
   }
 
   CreatePo(NewPo: POs) {
-    return this.http.post("https://macksdistribution.com/api/account/CreatePO", NewPo);
+    return this.http.post(this.ApiURL + "/CreatePO", NewPo);
   }
 
   Uploadfile(formData: FormData, FileName: string) {
-    return this.http.post('https://macksdistribution.com/api/account/UploadPO', formData, { 'params': { filename: FileName } });
+    return this.http.post(this.ApiURL + '/UploadPO', formData, { 'params': { filename: FileName } });
   }
 
   UpdatePo(UpdatedPo: POs) {
-    return this.http.post("https://macksdistribution.com/api/account/updatepo", UpdatedPo);
+    return this.http.post(this.ApiURL + "/updatepo", UpdatedPo);
   }
   GetProducts() {
-    return this.http.get("https://macksdistribution.com/api/account/GetCatalog");
+    return this.http.get(this.ApiURL + "/GetCatalog");
   }
 
   DeletePo(Po: POs) {
     Po.deleted = confirm("Are you Sure you Want To Delete this Po?")
-    return this.http.post("https://macksdistribution.com/api/account/updatepo", Po);
+    return this.http.post(this.ApiURL + "/updatepo", Po);
   }
 }
